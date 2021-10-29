6a2c06877a447c5a2c7108172e69abfb6ef9e795
Ustym Buchko <ustym.buchko1@gmail.com>
2021-10-29 15:16:54 +0300

    changing array name, making its' value constant

--- laba3.c
+++ laba3.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
 void main(void){
-    int n;
-    printf("Enter the size of array: \n");
-    scanf("%d", &n);
-    int mass[n];
-    for(int i = 0; i < n; i++){
-        scanf("%d", &mass[i]);
-    }
+    int n = 5;
+    int arr[n];
     int res;
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
     for (int i = 0; i < n; i++){
-        res = res ^ mass[i];
+        res = res ^ arr[i];
     }
     printf("The alone number is %d", res);
 }
